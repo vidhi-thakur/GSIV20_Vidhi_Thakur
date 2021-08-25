eb999c8e84b124d3169932d6222d3c394cb8dae7
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-08-25 16:52:29 +0530

    initialized new components and pages

---
+++ movies-browser/src/pages/DetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailsPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DetailsPage
---
+++ movies-browser/src/pages/ListPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListPage() {
+    return (
+        <div>
+            List page
+        </div>
+    )
+}
+
+export default ListPage
