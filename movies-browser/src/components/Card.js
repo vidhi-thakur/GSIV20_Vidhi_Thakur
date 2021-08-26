898b38a9ab31fe24eaacca97c1ec7007fc7cce25
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-08-27 03:51:25 +0530

    feat: updated card vomponent, list page & fetched data

--- movies-browser/src/components/Card.js
+++ movies-browser/src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Card() {
+function Card({ img, title, rating, description }) {
     return (
-        <div>
-            
+        <div className="w-44 bg-gray-100 border-gray-300 h-auto my-4 mx-6 rounded-lg shadow">
+            <img className="w-full h-40 rounded-t-lg" src={img} alt={`${title}-img`} />
+            <div>
+                <div className="flex justify-between items-center px-2 pt-2">
+                    <h3 className="truncate font-semibold">{title}</h3><span>{rating}</span>
+                </div>
+                <p className="truncate text-sm rounded-b-lg p-2">{description}</p>
+            </div>
         </div>
     )
 }
--- movies-browser/src/pages/ListPage.js
+++ movies-browser/src/pages/ListPage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import Card from '../components/Card'
+import React, { useEffect } from 'react'
 
 function ListPage() {
+    useEffect(() => {
+        getData()
+        return () => {
+        }
+    }, [])
+
+    const apiURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=ec2002ddbbe47acdd0bd8fb216ffc072&language=en-US";
+    function getData() {
+        fetch(apiURL).then(response => response.json()).then(responseJson => console.log(responseJson))
+    }
+
     return (
-        <div>
-            List page
+        <div className="flex flex-wrap justify-center pt-20">
+            <Card img="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" title="Wahh Taj" rating={8.5} description="An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage" />
+            <Card img="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" title="Wahh Taj" rating={8.5} description="An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage" />
+            <Card img="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" title="Wahh Taj" rating={8.5} description="An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage" />
+            <Card img="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" title="Wahh Taj" rating={8.5} description="An immense mausoleum of white marble, built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage" />
         </div>
     )
 }
