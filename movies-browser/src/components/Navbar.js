3caf62960dbc8ee3ac4d7c6a1b0e3cef155f2503
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-08-27 03:49:27 +0530

    style: navbar

--- movies-browser/src/components/Navbar.js
+++ movies-browser/src/components/Navbar.js
@@ -4,12 +4,12 @@ import HomeIcon from '@material-ui/icons/Home';
 
 function Navbar() {
     return (
-        <nav>
-            <div>
+        <nav className="fixed bg-white w-full max-w-4xl flex flex-row items-center justify-between py-2 px-3 border-b-gray-300 border-b-2 z-10 border-b-solid boxShadow-bottom">
+            <div className="flex flex-row items-center justify-between bg-gray-300 px-2 py-1 rounded-md pr-8">
                 <SearchIcon />
-                <input type="text" />
+                <input placeholder="Search" type="text" className="bg-transparent p-1 outline-none flex-1" />
             </div>
-            <HomeIcon />
+            <HomeIcon className="cursor-pointer" />
         </nav>
     )
 }
