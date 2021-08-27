cb11a651e4c0382a8c48e4002e63e6ad8e8c4dfa
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-08-28 04:07:49 +0530

    feat: added search functionality

--- movies-browser/src/components/Navbar.js
+++ movies-browser/src/components/Navbar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from 'react-router-dom';
+import { useStateValue } from "../StateProvider"
 
 function Navbar() {
+
+    const [, dispatch] = useStateValue();
+
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        dispatch({
+            type: "SET_INPUT",
+            input: input
+        })
+    }, [input])
+
     return (
         <nav className="fixed bg-white w-full max-w-4xl flex flex-row items-center justify-between py-2 px-3 border-b-gray-300 border-b-2 z-10 border-b-solid boxShadow-bottom">
             <div className="flex flex-row items-center justify-between bg-gray-300 px-2 py-1 rounded-md pr-8">
                 <SearchIcon />
-                <input placeholder="Search" type="text" className="bg-transparent p-1 outline-none flex-1" />
+                <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Search" type="text" className="bg-transparent p-1 outline-none flex-1" />
             </div>
             <Link to="/" >
                 <HomeIcon className="cursor-pointer" />
--- movies-browser/src/pages/ListPage.js
+++ movies-browser/src/pages/ListPage.js
@@ -1,29 +1,51 @@
-import Card from '../components/Card'
 import React, { useEffect, useState } from 'react'
+import Card from '../components/Card'
+import { useStateValue } from '../StateProvider'
 
 function ListPage() {
     const [dataArray, setDataArray] = useState([])
+    const [initialData, setInitialData] = useState([])
+
+    const [{input}] = useStateValue();
+
     useEffect(() => {
         getData();
         return () => {
-            getData()
+            getData();
         }
     }, [])
+    
+    useEffect(() => {
+        searchData(input)
+        return () => {
+        }
+    }, [input])
 
     const apiURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=ec2002ddbbe47acdd0bd8fb216ffc072&language=en-US";
     function getData() {
         fetch(apiURL).then(response => response.json()).then(responseJson => {
             setDataArray(responseJson.results);
-            console.log(responseJson.results)
+            setInitialData(responseJson.results);
         })
     }
 
+    function searchData(text) {
+        if (text === "") {
+            setDataArray(initialData);
+        } else {
+            let temp = dataArray.filter(item => item.title.toLowerCase().includes(text.toLowerCase()));
+            console.log("temp >>>", temp)
+            setDataArray(temp);
+            console.log("dataArray >>>",dataArray)
+        }
+    }
+
     return (
-        <div className="flex flex-wrap justify-center pt-20">
+        <div className="flex flex-wrap justify-left pt-20">
             {
                 dataArray.map(d => {
                     return (
-                        <Card img={`https://image.tmdb.org/t/p/w500${d.poster_path}`} title={d.original_title} rating={d.vote_average} description={d.overview} id={d.id} />
+                        <Card img={`https://image.tmdb.org/t/p/w500${d.poster_path}`} title={d.original_title} rating={d.vote_average} description={d.overview} id={d.id} key={d.id} release_date={d.release_date} searchData={searchData} />
                     );
                 })
             }
