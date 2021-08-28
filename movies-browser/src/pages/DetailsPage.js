772886f0dd4599fb519ffad4d96090230bc5c805
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-08-28 20:46:57 +0530

    added responsiveness to details page

--- movies-browser/src/pages/DetailsPage.js
+++ movies-browser/src/pages/DetailsPage.js
@@ -16,18 +16,18 @@ function DetailsPage() {
     function getDetails() {
         const detailsUrl = `https://api.themoviedb.org/3/movie/${details.id}/credits?api_key=ec2002ddbbe47acdd0bd8fb216ffc072&language=en-US`;
         fetch(detailsUrl).then(res => res.json()).then(resJson => {
-            resJson.case.map(item => (setCast(cast => [...cast, item.name])))
+            resJson.cast.map(item => (setCast(cast => [...cast, item.name])))
         })
     }
 
     return (
-        <div className="pt-20 h-72 px-4 flex justify-evenly">
-            <img className="w-1/4 h-full" src={details.img} alt="poster" />
-            <div className="w-3/4 h-full py-2 px-4 flex flex-col justify-between">
-                <h1 className="font-semibold">{details.title}</h1>
-                <p> {details.release_date} | 02:33 | Mark</p>
-                <p>Cast: {cast.map(c => <span>{c}, </span>)}</p>
-                <p className="text-justify">Description: {details.description}</p>
+        <div className="pt-20 h-auto px-4 flex flex-col sm:flex-row justify-evenly">
+            <img className="sm:w-1/4 w-full max-h-h2 sm:max-h-h1" src={details.img} alt="poster" />
+            <div className="sm:w-3/4 w-full py-2 sm:px-4 px-0 flex flex-col justify-between">
+                <h1 className="font-semibold text-4xl">{details.title}</h1>
+                <p className="mt-4"> Release date: {details.release_date}</p>
+                <p className="mt-4 text-justify truncate">Cast: {cast.map(c => <span>{c}, </span>)}</p>
+                <p className="text-justify mt-4">Description: {details.description}</p>
             </div>
         </div>
     )
